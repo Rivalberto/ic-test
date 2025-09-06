8358f70f6cb0b85ef6d132ed0e1b3d73098a96e4
Rivalberto <127450828+Rivalberto@users.noreply.github.com>
2025-09-06 23:55:49 +0200

    Update app5.py

--- app5.py
+++ app5.py
@@ -173,7 +173,8 @@ def main():
     MAC_station = '70:ee:50:64:49:34' # MAC address of the device looks like: '21:ff:31:69:2d:19'
     MAC_station_module_ext = '02:00:00:65:33:f2'
     
-    date_start = datetime.now()-timedelta(minutes=10)
+    #date_start = datetime.now()-timedelta(minutes=10)
+    date_start = datetime.now()+timedelta(minutes=10)
     date_start = int(date_start.replace(tzinfo=None).timestamp())
     #date_end = int(datetime.now().timestamp())
     
