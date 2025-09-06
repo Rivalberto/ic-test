6a9e882d894fd2478a6698791275a1a4a7e4e13d
Rivalberto <127450828+Rivalberto@users.noreply.github.com>
2025-09-07 00:25:24 +0200

    Update app5.py

--- app5.py
+++ app5.py
@@ -304,7 +304,7 @@ def main():
     st.write(f"Pressione atmosferica: {p_atm:.1f} hPA")
     
     if not (0 <= rh_indoor <= 100 and 0 <= rh_outdoor <= 100):
-        st.write("Errore: L'umidità relativa deve essere tra 0 e 100%.")
+        st.error("Errore: L'umidità relativa deve essere tra 0 e 100%.")
         return
 
     st.subheader("Dati calcolati")
@@ -332,7 +332,7 @@ def main():
         st.write(f"Umidità Assoluta Interna: {abs_hum_indoor:.2f} g/m³")
         st.write(f"Umidità Assoluta Esterna: {abs_hum_outdoor:.2f} g/m³")
     else:
-        st.write("Impossibile calcolare l'umidità assoluta a causa di errori precedenti.")
+        st.error("Impossibile calcolare l'umidità assoluta a causa di errori precedenti.")
 
     # Calcola e stampa il rapporto di miscela (w) e l'umidità specifica (q)
     if pv_indoor_val is not None and pv_outdoor_val is not None:
@@ -349,7 +349,7 @@ def main():
         st.write(f"Rapporto di Miscela Esterno (w_outdoor): {w_outdoor:.2f} g/kg")
             
     else:
-        st.write("Impossibile calcolare rapporto di miscela e umidità specifica a causa di errori precedenti.")
+        st.error("Impossibile calcolare rapporto di miscela e umidità specifica a causa di errori precedenti.")
 
 
     # Indicatore visivo testuale e colore per grafico
@@ -360,17 +360,16 @@ def main():
             verdict = "Apri"
             verdict_color = 'green'
             st.success("✅ L'aria esterna è più secca: aprendo le finestre ridurrai l'umidità interna.")
+        elif ratio_value < 1:
+            verdict = "Chiudi"
+            verdict_color = 'red'
+            st.error("❌ L'aria esterna è più umida: aprendo le finestre aumenterai l'umidità interna.")
         else:
-            if ratio_value < 1:
-                verdict = "Chiudi"
-                verdict_color = 'red'
-                st.error("❌ L'aria esterna è più umida: aprendo le finestre aumenterai l'umidità interna.")
-            else:
-                #verdict = "Uguale"
-                #verdict_color = 'blue'
-                #st.info("ℹ️ L'aria esterna e interna hanno la stessa umidità specifica.")
+            verdict = "Uguale"
+            verdict_color = 'blue'
+            st.info("ℹ️ L'aria esterna e interna hanno la stessa umidità specifica.")
     else:
-        st.write("Impossibile calcolare il rapporto (w_indoor / w_outdoor) a causa di condizioni non valide o estreme.")
+        st.warning("Impossibile calcolare il rapporto (w_indoor / w_outdoor) a causa di condizioni non valide o estreme.")
 
     #except ValueError:
     #    st.write("\nErrore: Assicurati di inserire valori numerici validi per temperature, umidità e pressione.")
