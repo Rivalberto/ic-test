a11a36051ad6cd015e05e37bcfcf3c4eb78ca345
Rivalberto <127450828+Rivalberto@users.noreply.github.com>
2025-09-07 14:33:47 +0200

    Update app5.py

--- app5.py
+++ app5.py
@@ -320,12 +320,12 @@ def main():
     
     response = requests.get(url=URL, params=params, headers=headers)
     #print(response.content)
-    st.write(response.content)
+    #st.write(response.content)
     
     body = response.json()['body']
     values_station = dict()
     for key in body:
-        values_station["time"] = datetime.fromtimestamp(int(key))
+        values_station["time"] = datetime.fromtimestamp(int(key)).time
         values_station["temperature"] = body[key][0]
         values_station["humidity"] = body[key][1]
         values_station["pressure"] = body[key][2]
