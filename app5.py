944682c1ac618d1df8b8143ca69046879105b3ca
Rivalberto <127450828+Rivalberto@users.noreply.github.com>
2025-09-15 14:01:17 +0200

    Update app5.py

--- app5.py
+++ app5.py
@@ -198,8 +198,7 @@ def main():
     github_token = st.secrets.github.token
 
     #URL = 'https://raw.githubusercontent.com/Rivalberto/ic-test/refs/heads/main/tokens.csv'
-    #URL = 'https://api.github.com/repos/Rivalberto/ic-test/branches/main'
-    URL = 'https://api.github.com/repos/Rivalberto/ic-test/git'
+    URL = 'https://api.github.com/repos/Rivalberto/ic-test/branches/main/tree'
     response = requests.get(URL, auth=(github_user,github_token))
     st.write(response.status_code)
     if response.status_code == 200:
