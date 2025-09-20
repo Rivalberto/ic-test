db01873bd697debc2dc374c9f22129f976137eb3
Rivalberto <127450828+Rivalberto@users.noreply.github.com>
2025-09-20 11:20:27 +0200

    Update app5.py

--- app5.py
+++ app5.py
@@ -296,7 +296,9 @@ def main():
         #}
         
         content = 'Access,Refresh,Expiration,Scope,Lock\n', st.session_state['Access'], ',', st.session_state['Refresh'], ',', st.session_state['Expiration'], ',', st.session_state['Scope'], ',', st.session_state['Lock']
+        st.write(content)
         content = base64.b64encode(content)
+        st.write(content)
         
         inputdata = {}
         inputdata["path"] = "tokens.csv"
